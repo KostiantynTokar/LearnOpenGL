f70b1efc176ce225bf830012d09967da685333a8
Kostiantyn Tokar <tokar.kostya@gmail.com>
2021-01-21 20:37:43 +0200

    Remove _yaw and _pitch fields from Camera struct

--- source/glsu/abstractions.d
+++ source/glsu/abstractions.d
@@ -31,18 +31,17 @@ struct Camera
      * Params:
      *   position = 3D position coordinates of the camera.
      *   yaw = Yaw Euler angle in radians, default is `-PI/2` (so camera looks at negative z-axis).
-     *         Wrapped to interval [-PI, PI$(RPAREN).
+     *         Wrapped to interval [-PI, PI$(RPAREN), grows anticlockwise at xz plane.
      *   pitch = Pitch Euler angle in radians, default is `0` (so camera looks perpendicularly y-axis).
-     *           Clamped to interval [-radians(89.0f), radians(89.0f)].
+     *           Clamped to interval [-radians(89.0f), radians(89.0f)], grows anticlockwise at yz plane.
      *   worldUp = Constant to determine camera orientation angles.
      */
     this(vec3f position, float yaw = -PI_2, float pitch = 0.0f,
-         vec3f worldUp = vec3f(0.0f, 1.0f, 0.0f)) nothrow @nogc @safe
+         vec3f worldUp = vec3f(0.0f, 1.0f, 0.0f)) pure nothrow @nogc @safe
     {
         _position = position;
-        _worldUp = worldUp;
-        updateAngles(yaw, pitch);
-        updateVectors();
+        _worldUp = worldUp.normalized;
+        updateVectors(yaw, pitch);
     }
 
     /// 3D position coordinates of the camera.
@@ -76,13 +75,13 @@ struct Camera
     /// Yaw Euler angle in radians. Wrapped to interval [-PI, PI$(RPAREN).
     float yaw() const pure nothrow @nogc @safe
     {
-        return _yaw;
+        return calcYawFromOrigin(front);
     }
 
     /// Pitch Euler angle in radians. Clamped to interval [-radians(89.0f), radians(89.0f)].
     float pitch() const pure nothrow @nogc @safe
     {
-        return _pitch;
+        return calcPitchFromOrigin(front);
     }
 
     /// Calculates view matrix of the camera.
@@ -122,24 +121,16 @@ struct Camera
     }
 
     /// Rotates using Euler angles in radians.
-    void rotate(float yawOffset, float pitchOffset) nothrow @nogc @safe
+    void rotate(float yawOffset, float pitchOffset) pure nothrow @nogc @safe
     {
-        updateAngles(yaw + yawOffset, pitch + pitchOffset);
-        updateVectors();
+        updateVectors(yaw + yawOffset, pitch + pitchOffset);
     }
 
     /// Change yaw and pitch so that front points to `point`.
-    void lookAt(vec3f point) nothrow @nogc @safe
+    void lookAt(vec3f point) pure nothrow @nogc @safe
     {
-        import std.math : atan2, hypot;
-
-        immutable diff = position - point;
-
-        immutable newYaw = atan2(diff.z, diff.x) - PI;
-        immutable newPitch = -atan2(diff.y, cast(float) hypot(diff.x, diff.z));
-
-        updateAngles(newYaw, newPitch);
-        updateVectors();
+        immutable newFront = (point - position).normalized;
+        updateVectors(newFront);
     }
     ///
     unittest
@@ -163,21 +154,8 @@ private:
     vec3f _up;
     vec3f _right;
     vec3f _worldUp;
-    float _yaw;
-    float _pitch;
 
-    void updateAngles(float newYaw, float newPitch) nothrow @nogc @safe
-    out(; -PI <= _yaw && _yaw < PI)
-    out(; -radians(89.0f) <= _pitch && _pitch <= radians(89.0f))
-    {
-        import std.algorithm.comparison : clamp;
-        import gfm.math.funcs : radians;
-
-        _yaw = constrainAngle(newYaw);
-        _pitch = clamp(newPitch, -radians(89.0f), radians(89.0f));
-    }
-
-    void updateVectors() pure nothrow @nogc @safe
+    void updateVectors(float newYaw, float newPitch) pure nothrow @nogc @safe
     out(; _front.magnitude.approxEqual(1.0f))
     out(; _right.magnitude.approxEqual(1.0f))
     out(; _up.magnitude.approxEqual(1.0f))
@@ -188,15 +166,48 @@ private:
         import std.math : sin, cos;
         import gfm.math.vector : cross;
 
-        immutable sp = sin(pitch);
-        immutable cp = cos(pitch);
+        immutable t = constrainAngles(newYaw, newPitch);
 
-        immutable sy = sin(yaw);
-        immutable cy = cos(yaw);
+        immutable sp = sin(t.pitch);
+        immutable cp = cos(t.pitch);
+
+        immutable sy = sin(t.yaw);
+        immutable cy = cos(t.yaw);
 
         _front = vec3f(cy * cp, sp, sy * cp).normalized;
         _right = cross(_front, _worldUp).normalized;
         _up = cross(_right, _front).normalized;
+    }
+
+    void updateVectors(vec3f newFront) pure nothrow @nogc @safe
+    in(newFront.magnitude.approxEqual(1.0f))
+    out(; _front.magnitude.approxEqual(1.0f))
+    out(; _right.magnitude.approxEqual(1.0f))
+    out(; _up.magnitude.approxEqual(1.0f))
+    out(; dot(_front, _up).approxEqual(0.0f))
+    out(; dot(_front, _right).approxEqual(0.0f))
+    out(; dot(_right, _up).approxEqual(0.0f))
+    {
+        import gfm.math.vector : cross;
+
+        _front = newFront;
+        _right = cross(_front, _worldUp).normalized;
+        _up = cross(_right, _front).normalized;
+    }
+
+    import std.typecons : Tuple;
+    /// Wraps `yaw` in [-PI, PI) and clamps `pitch` to [-89.radians, 89.radians].
+    static Tuple!(float, "yaw", float, "pitch") 
+        constrainAngles(float yaw, float pitch) pure nothrow @nogc @safe
+    out(res; -PI <= res.yaw && res.yaw < PI)
+    out(res; -radians(89.0f) <= res.pitch && res.pitch <= radians(89.0f))
+    {
+        import std.typecons : tuple;
+        import std.algorithm : clamp;
+        import gfm.math.funcs : radians;
+
+        return tuple!("yaw", "pitch")(wrapAngle(yaw),
+                                      clamp(pitch, -radians(89.0f), radians(89.0f)));
     }
 
     /** 
@@ -205,7 +216,7 @@ private:
      *   angle = Value to wrap.
      * Returns: Value in interval [-PI, PI).
      */
-    static float constrainAngle(float angle) pure nothrow @nogc @safe
+    static float wrapAngle(float angle) pure nothrow @nogc @safe
     {
         angle = (angle + PI) % (2 * PI);
         if (angle < 0)
@@ -217,22 +228,36 @@ private:
     ///
     unittest
     {
-        assert(Camera.constrainAngle(0.0f).approxEqual(0.0f));
+        assert(Camera.wrapAngle(0.0f).approxEqual(0.0f));
         
-        assert(Camera.constrainAngle(PI_2).approxEqual(PI_2));
-        assert(Camera.constrainAngle(PI + PI_2).approxEqual(-PI_2));
-        assert(Camera.constrainAngle(2 * PI + PI_2).approxEqual(PI_2));
+        assert(Camera.wrapAngle(PI_2).approxEqual(PI_2));
+        assert(Camera.wrapAngle(PI + PI_2).approxEqual(-PI_2));
+        assert(Camera.wrapAngle(2 * PI + PI_2).approxEqual(PI_2));
 
-        assert(Camera.constrainAngle(-PI_2).approxEqual(-PI_2));
-        assert(Camera.constrainAngle(-PI - PI_2).approxEqual(PI_2));
-        assert(Camera.constrainAngle(-2 * PI - PI_2).approxEqual(-PI_2));
+        assert(Camera.wrapAngle(-PI_2).approxEqual(-PI_2));
+        assert(Camera.wrapAngle(-PI - PI_2).approxEqual(PI_2));
+        assert(Camera.wrapAngle(-2 * PI - PI_2).approxEqual(-PI_2));
+    }
+
+    /// Return yaw between (0,0,0) and `point`.
+    static float calcYawFromOrigin(vec3f point) pure nothrow @nogc @safe
+    {
+        import std.math : atan2;
+        return atan2(-point.z, -point.x) - PI;
+    }
+
+    /// Return pitch between (0,0,0) and `point`.
+    static float calcPitchFromOrigin(vec3f point) pure nothrow @nogc @safe
+    {
+        import std.math : atan2, hypot;
+        return atan2(point.y, cast(float) hypot(point.x, point.z));
     }
 }
 ///
 unittest
 {
-    import gfm.math : vec3f, vec4f;
-    import std.math : PI_2, approxEqual;
+    import gfm.math : vec3f, vec4f, mat3f;
+    import std.math : PI_2, PI_4, approxEqual;
 
     vec3f fromHomogeneous(vec4f v)
     {
@@ -253,4 +278,7 @@ unittest
 
     camera.moveFront(1.0f);
     assert(applyView(camera, origin)[].approxEqual([-1.0f, 0.0f, 1.0f]));
+
+    camera.rotate(0.0f, PI_4);
+    assert(applyView(camera, origin)[].approxEqual((mat3f.rotateX(-PI_4) * vec3f(-1.0f, 0.0f, 1.0f))[]));
 }
